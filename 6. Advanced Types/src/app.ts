11a02343d3d80455c622817bbbec9600faa5f8b1
Cristian Delcid <cristian.delcid10@gmail.com>
2020-12-10 16:34:59 -0600

    build: advanced-types added

---
+++ 6. Advanced Types/src/app.ts
@@ -0,0 +1,121 @@
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: string;
+};
+
+type ElevatedEmployees = Admin & Employee;
+
+const e1: ElevatedEmployees = {
+    name: 'Cristian Delcid',
+    privileges: [ 'create-server' ],
+    startDate: new Date().toLocaleDateString('hn-HN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).toUpperCase()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric; // Intersection Types
+
+function add( a: Combinable, b: Combinable ) {
+    if ( typeof a === 'string' || typeof b === 'string' ) {
+        return a.toString() + ' ' + b.toString();
+    }
+    return a + b;
+}
+
+console.log( add( 'Hola', 'Mundo' ) );
+console.log( add( 4, 29 ) );
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation( emp: UnknownEmployee ) {
+    console.log( 'Name:', emp.name );
+    if ( 'privileges' in emp ) {
+        console.log( 'Privileges:', emp.privileges );
+    }
+    if ( 'startDate' in emp ) {
+        console.log( 'Start Date:', emp.startDate );
+    }
+};
+
+printEmployeeInformation( e1 );
+
+class Car {
+    drive() {
+        console.log( 'Driving...' )
+    }
+}
+
+class Truck {
+    drive() {
+        console.log( 'Driving a truck...' );
+    }
+
+    loadCargo( amount: number ) {
+        console.log( 'Loading cargo... ' + amount );
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle( vehicle: Vehicle ) {
+    vehicle.drive();
+    if ( vehicle instanceof Truck ) {
+        vehicle.loadCargo( 5000 );
+    }
+}
+
+useVehicle( v1 );
+useVehicle( v2 );
+
+// Discriminated Unions
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal( animal: Animal ) {
+    let speed;
+    switch ( animal.type ) {
+        case 'bird':
+            speed = animal.flyingSpeed
+            break;
+        case 'horse':
+            speed = animal.runningSpeed
+    }
+    console.log(`Moving at speed: ${ speed }`);
+};
+
+moveAnimal({ type: 'bird', flyingSpeed: 20 });
+moveAnimal({ type: 'horse', runningSpeed: 24 });
+
+// Type Casting
+// const userInput = document.getElementById('user-input') as HTMLInputElement;
+const userInput = <HTMLInputElement>document.getElementById('user-input')!;
+
+userInput.value = 'Hola Mundo'
+
+// Index Properties
+interface ErrorContainer {
+    [ prop: string ]: string;
+}
+
+const errorBar: ErrorContainer = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character!'
+}
