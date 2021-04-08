03b9971893677c74d013104aaf606a19a17d22b9
Cristian Delcid <cristian_delcid10@yahoo.com>
2021-04-08 17:44:58 -0600

    build: Webpack and configurations added

---
+++ 9. ProjectManager/webpack.config.prod.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+   entry: './src/app.ts',
+   mode: 'production',
+   module: {
+      rules: [
+         {
+            test: /\.ts?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   resolve: {
+      extensions: ['.ts', '.js'],
+   },
+   output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+   },
+   plugins: [new CleanWebpackPlugin()],
+};
